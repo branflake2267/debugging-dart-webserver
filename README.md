994daf0afe8de3bba04c86dd334424d92789d63f
Brandon Donnelson <branflake2267@gmail.com>
2020-05-16 22:04:15 -0700

    Initial config

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # debugging-dart-webserver
-Debugging a Dart HTTP web server
+Debugging a Dart HTTP web server.
+
+
+## Debugging
+
+* Run `pub get` to initialize the project.
+
+## Debugging with Dart CLI
+
+* Run `dart ./lib/server.dart`
+
+## Debugging with VS Code
+
+* Run the VS Code Launcher `Launch Web Server`
+
+
+
+## Init Reference
+
+### Install Dart
+The dart tools are required to use this project. 
+
+* [Install Dart](https://dart.dev/get-dart)
+
+### Install VS Code Dart Plugin
+Install the Dart VS Code plugin. 
+
+Add the program launcher, to launch the web server.
+```
+{
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "name": "Launch Web Server",
+      "program": "lib/server.dart",
+      "request": "launch",
+      "type": "dart"
+    }
+  ]
+}
+```
+
---
+++ lib/server.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+/// Use a VS Code launcher to launch this
+/// OR use dart CLI Run `dart ./lib/server.dart`
+main() async {
+  var server = await HttpServer.bind(InternetAddress.loopbackIPv4, 8080);
+  print("Serving at ${server.address}:${server.port}");
+
+  await for (var request in server) {
+    handleRequest(request);
+  }
+}
+
+void handleRequest(HttpRequest request) {
+  try {
+    if (request.method == 'GET') {
+      handleGet(request);
+    } else {
+       request.response
+        ..statusCode = HttpStatus.methodNotAllowed
+        ..write('Unsupported request: ${request.method}.')
+        ..close();
+    }
+  } catch (e) {
+    print('Exception in handleRequest: $e');
+  }
+}
+
+void handleGet(HttpRequest request) {
+  final uri = request.uri;
+  if (uri.path == '/api') {
+     request.response
+      ..headers.contentType = ContentType.json
+      ..write('{ "Message": "The board is green." }')
+      ..close();
+  } else {
+      request.response
+      ..headers.contentType = ContentType.text
+      ..write('Works...')
+      ..close();
+  }
+}
